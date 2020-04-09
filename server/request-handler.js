58ceb1ff806e5e5cbc7e41812028d6556b0ef1bd
sean_park <19980819@naver.com>
2020-04-09 13:38:43 +0900

    .

--- server/request-handler.js
+++ server/request-handler.js
@@ -8,7 +8,9 @@ requestHandler 함수를 export 하여 basic-server.js 에서 사용 할 수 있
 
 **************************************************************/
 
-const requestHandler = function(request, response) {
+const requestHandler = function (request, response) {
+  let chats = [];
+  let id = 0;
   // node server 의 requestHandler는 항상 request, response를 인자로 받습니다.
 
   // 또한 http 요청은 항상 요청과 응답이 동반 되어야 합니다.
@@ -19,12 +21,13 @@ const requestHandler = function(request, response) {
   //
   // 간단한 로그를 작성 하는 것은, 서버를 디버깅 하는데 매우 수월하게 해줍니다.
   // 아래는 모든 리퀘스트의 메소드와 url을 로깅 해줍니다.
+  const statusCode = 200;
+
   console.log(
     "Serving request type " + request.method + " for url " + request.url
   );
 
   // 응답을 위한 status 코드입니다.
-  const statusCode = 200;
 
   // 기본 CORS 설정이 되어있는 코드 입니다. 아래에 있습니다.
   // CORS에 대해서는 조금더 알아보세요.
@@ -33,9 +36,44 @@ const requestHandler = function(request, response) {
   headers["Content-Type"] = "text/plain";
 
   // .writeHead() 메소드는 응답 헤더에 해당 key, value 를 적어줍니다.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
 
   // 노드 서버에 대한 모든 요청은 응답이 있어야 합니다. response.end 메소드는 요청에 대한 응답을 보내줍니다.
+
+  // if (request.url !== "/classes/messages") {
+  //   response.writeHead(404, headers);
+  //   response.end()
+  // }
+
+  if (request.method === "OPTIONS") {
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+  if (request.method === "GET") {
+    if (request.url !== "/classes/messages") {
+      response.writeHead(404, headers);
+      response.end(chats.toString());
+    } else {
+      response.writeHead(statusCode, headers);
+      response.end(chats.toString());
+    }
+  }
+  if (request.method === "POST") {
+    // if (request.url !== "/classes/messages") {
+    //   response.writeHead(404, headers);
+    //   response.end();
+    // } else {
+      request.on('data', chunk => {
+        let chat = JSON.parse(chunk.toString());
+        chat.id = id;
+        chat.date = new Date();
+        chats.push(chat);
+        id++;
+        response.writeHead(201, headers);
+        response.end(chats[id].id.toString());
+      });
+    
+  }
   response.end("Hello, World!");
 };
 
@@ -54,3 +92,5 @@ const defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+module.exports = requestHandler;
